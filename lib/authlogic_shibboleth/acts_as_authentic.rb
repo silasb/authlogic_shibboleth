61d857cba6f10a42184078daf61bceb643e66eaf
Stafford Brunk <wingrunr21@gmail.com>
2010-11-06 19:55:22 -0400

    Gem now correctly loads REMOTE_USER and saves session.  There is a problem with UserSession.find.  I think it has something to do with credentials

--- lib/authlogic_shibboleth/acts_as_authentic.rb
+++ lib/authlogic_shibboleth/acts_as_authentic.rb
@@ -8,15 +8,20 @@ module AuthlogicShibboleth
 		end
 		
 		module Config
+			def validate_shibboleth_login(value=nil)
+				config(:validate_shibboleth_login, value, true)
+			end
+			alias_method :validate_shibboleth_login=, :validate_shibboleth_login
 		end
 		
 		module Methods
 			def self.included(klass)
-				return if !klass.column_names.include?("shibboleth_id")
+				return if !klass.column_names.include?("shibboleth_id") #we need to store our ids in the database
         
         klass.class_eval do
           validates_uniqueness_of :shibboleth_id, :scope => validations_scope
         end
+      end
 		end
 	end
 end
--- lib/authlogic_shibboleth/session.rb
+++ lib/authlogic_shibboleth/session.rb
@@ -12,40 +12,60 @@ module AuthlogicShibboleth
 				rw_config(:finx_by_shibboleth_id_method, value, :find_by_shibboleth_id)
 			end
 			alias_method :find_by_shibboleth_id_method=, :find_by_shibboleth_id_method
-			
-			def auto_register(value=true)
-        auto_register_value(value)
-      end
-      
-      def auto_register_value(value=nil)
-        rw_config(:auto_register,value,false)
-      end
-      
-      alias_method :auto_register=,:auto_register
 		end
 		
 		module Methods
 			def self.included(klass)
         klass.class_eval do
           attr_reader :shibboleth_id
+          validate :validate_by_shibboleth_id, :if => :authenticating_with_shibboleth_id?
+          
+          #Because we are dealing with Shibboleth, the id comes to us via
+          #the request's REMOTE_USER variable
+          def shibboleth_id
+            controller.request.env['REMOTE_USER']
+          end
         end
       end
       
-      def verify_session_with_env
-    	end
+      #TODO: I am not sure this behavior is correct.
+      def credentials
+        if authenticating_with_shibboleth_id?
+          values = [:shibboleth_id]
+        else
+          super
+        end
+      end
       
 			private
+			
+			def authenticating_with_shibboleth_id?
+			  shibboleth_id_defined?
+		  end
+		  
+		  #REMOTE_USER will return blank if it is not set
+		  def shibboleth_id_defined?
+		    !shibboleth_id.blank?
+	    end
+		  
+	    #Attempts to find the given ID in the database
+	    #TODO: create some type of auto-registration system
+	    #      if the id is not found
+		  def validate_by_shibboleth_id
+		    self.attempted_record = klass.send(find_by_shibboleth_id_method, shibboleth_id)
+		    
+		    unless self.attempted_record
+		      errors.add_to_base(I18n.t('error_messages.shibboleth_id_not_found'), :default => "Your Shibboleth ID was not found.")
+		    end
+	    end
 			
 			def find_by_shibboleth_id_method
 				self.class.find_by_shibboleth_id_method
 			end
 			
-			#This method always returns true
-			#this it should only be called when the proper
-			#environment variable has been set
-			def validate_by_shibboleth_id
-				true
-			end
+			def shibboleth_id
+			  self.class.shibboleth_id
+		  end
 		end
 	end
 end
